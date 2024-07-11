0bdb6eae3d24e4c64f4088c2f55d079616df3806
kalz2q <katsu95i@gmail.com>
2024-07-11 09:43:33 +0900

    mm

--- app/79googleDrive00.tsx
+++ app/79googleDrive00.tsx
@@ -4,7 +4,12 @@ import { useState } from "react";
 export default function Home() {
   return (
     <div className="m-8">
-      <img src="https://lh3.google.com/u/0/d/1ouKwNK7a8O1SbxiNA6754Mekw4wqbq9r"></img>
+      <picture>
+        <img
+          src="https://lh3.google.com/u/0/d/1ouKwNK7a8O1SbxiNA6754Mekw4wqbq9r"
+          alt=""
+        ></img>
+      </picture>
     </div>
     // https://lh3.google.com/u/0/d/{ファイルID}=w{幅px}-h{高さpx}-iv1
   );
---
+++ app/81iframe.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="m-8">
+      <iframe
+        className="w-96 h-16"
+        src="https://drive.google.com/file/d/1UBsVhZHAFJZp0TXFAFin46CKQ8hrgL8Q/preview"
+      ></iframe>
+    </div>
+  );
+}
